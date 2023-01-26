e83a7adf981acacf77617e35ebd94bc16eab010d
Gabekwwl <gabekwwl@gmail.com>
2023-01-26 19:50:00 +0100

    dupa

---
+++ TicTacToe/move.py
@@ -0,0 +1,34 @@
+def ruch(plansza, gracz, kolumna, wiersz):
+    if kolumna > 2 or wiersz > 2 or plansza[wiersz][kolumna] != '[ ]':
+        print("Błędny ruch. Spróbuj ponownie.")
+        return gracz
+    plansza[wiersz][kolumna] = f'[{gracz}]'
+    gracz = zmianaGracza(gracz)
+    return gracz
+
+def zmianaGracza(gracz):
+    if gracz == 'X':
+        gracz = 'O'
+    elif gracz =='O':
+        gracz = 'X'
+    return gracz
+
+def czyKoniec(plansza):
+        if plansza[0][0] != "[ ]" and plansza[0][0] == plansza[0][1] and plansza[0][1] == plansza[0][2]:
+            return True
+        elif plansza[1][0] != "[ ]" and plansza[1][0] == plansza[1][1] and plansza[1][1] == plansza[1][2]:
+            return True
+        elif plansza[2][0] != "[ ]" and plansza[2][0] == plansza[2][1] and plansza[2][1] == plansza[2][2]:
+            return True
+        elif plansza[0][0] != "[ ]" and plansza[0][0] == plansza[1][0] and plansza[1][0] == plansza[2][0]:
+            return True
+        elif plansza[0][1] != "[ ]" and plansza[0][1] == plansza[1][1] and plansza[1][1] == plansza[2][1]:
+            return True
+        elif plansza[0][2] != "[ ]" and plansza[0][2] == plansza[1][2] and plansza[1][2] == plansza[2][2]:
+            return True
+        elif plansza[0][0] != "[ ]" and plansza[0][0] == plansza[1][1] and plansza[1][1] == plansza[2][2]:
+            return True
+        elif plansza[0][2] != "[ ]" and plansza[0][2] == plansza[1][1] and plansza[1][1] == plansza[2][0]:
+            return True
+        else:
+            return False
---
+++ TicTacToe/tictactoe.py
@@ -0,0 +1,27 @@
+'''
+0 - puste pole
+1 - oznacza gracz x
+2 - oznacza gracz o
+'''
+
+from board import *
+from move import *
+
+gracz = 'X'
+czyKoniecGry = False
+
+plansza = tworzPlansze()
+printPlansza(plansza, gracz)
+
+
+
+
+while czyKoniecGry == False :
+    kolumna = int(input("Podaj kolumnę do ruchu [0-2]: "))
+    wiersz = int(input("Podaj wiersz do ruchu [0-2]: "))
+    gracz = ruch(plansza, gracz, kolumna, wiersz)
+    printPlansza(plansza, gracz)
+    czyKoniecGry = czyKoniec(plansza)
+    if czyKoniecGry:
+        print("HURRA! Wygrałeś!")
+    
